7be7ded6d6b93cf9ab38a1e0381a253e8950b6b6
Anna Beddow <anna.beddow@gmail.com>
2016-11-29 11:35:15 +0000

    added withdraw method

--- lib/account.rb
+++ lib/account.rb
@@ -8,4 +8,20 @@ attr_reader :balance
    @balance = BALANCE
  end
 
+def deposit(amount)
+  @balance+= amount
+  print_current_balance
+end
+
+def withdraw(amount)
+  new_balance = @balance-amount
+  raise "Sorry, you have insufficient funds for this withdrawal" if new_balance < 0
+  @balance = new_balance
+  print_current_balance
+end
+
+def print_current_balance
+  print "Thank you. Your balance is now #{@balance}."
+end
+
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -9,8 +9,20 @@ describe Account do
       expect(account.balance).to eq(0)
     end
 
-    it "lets the user deposit 500 into account" do
+    it "lets the user deposit into account" do
       account.deposit(500)
       expect(account.balance).to eq(500)
     end
+
+    it "lets the user withdraw from account" do
+      account.deposit(500)
+      account.withdraw(300)
+      expect(account.balance).to eq(200)
+    end
+
+    it "raises error if user tries to withdraw more than they have available" do
+      account1 = Account.new
+      expect{account1.withdraw(20)}.to raise_error("Sorry, you have insufficient funds for this withdrawal")
+    end
+
 end
