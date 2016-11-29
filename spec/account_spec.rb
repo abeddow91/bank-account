8ddbb0c5126b5213b4e4b0396d91debc8c2bf9d2
Anna Beddow <anna.beddow@gmail.com>
2016-11-29 11:02:08 +0000

    added deposit test

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,8 +1,16 @@
 require 'account.rb'
 
+
 describe Account do
+
+  subject(:account) {described_class.new}
+
     it "creates an account with an empty balance" do
-      @account = Account.new
-      expect(@account.balance).to eq(0)
-  end
+      expect(account.balance).to eq(0)
+    end
+
+    it "lets the user deposit 500 into account" do
+      account.deposit(500)
+      expect(account.balance).to eq(500)
+    end
 end
